c21df188a1893592a7ece5b4023ca98336b202d3
junghwichan3436 <https://github.com/junghwichan3436/kdtfed-241219.git>
2025-01-15 20:44:17 +0900

    250115

--- 250114/Flex-project/script.js
+++ 250114/Flex-project/script.js
@@ -1,15 +1,19 @@
-//목표 : 사용자가 스크롤을 내렸을 때, header 배경컬러 // banner height를 변경
-// => 가상 클래스 적용
+// 목표 : 사용자가 스크롤을 내렸을 때, header 배경컬러 // banner height를 변경 => 가상클래스 적용
 
-//1. 사용작가 지금 스크롤을 내렸어!!
-//>스크롤은 브라우저가 가지고 있는 요소!!
+// 1.사용자가 지금 스크롤을 내렸어!!
+// > 스크롤은 브라우저가 가지고 있는 요소!!
 // > 브라우저야!!! 지금 스크롤이 내려갔니?
 
-//2. header는 html안에서 header라는 태그를 사용하고 있는 요소야!
+// 2.header는 html안에서 header라는 태그를 사용하고 있는 요소야!
 
-//3. 사용자가 스크롤을 내렸으니까 빨리  active 클래스를 header에게 적용시켜
+// 3.사용자가 스크롤을 내렸으니까 빨리 active 클래스를 header에게 적용시켜
 
-// scroll event
+// 브라우저가 가지고 있는 내장객체
+
+// document.querySelector("#circle");
+// 문서 => 개발자가 직접 작성한 html 문서
+
+/* scroll event */
 window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
     document.querySelector("header").classList.add("active");
@@ -17,7 +21,14 @@ window.addEventListener("scroll", () => {
     document.querySelector("header").classList.remove("active");
   }
 });
-//브라우저가 가지고 있는 내장객체
 
-// 문서 => 개발자가 직접 지정한 html 문서
-// document.querySelector("#circle");
+/* slick slider */
+$(document).ready(function () {
+  $(".myslider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    infinite: false,
+    dots: true,
+    speed: 300,
+  });
+});
